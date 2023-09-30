4773d77712c7a21e97d1f7cf0cd737e0fcb29044
Baptiste Demorgny <baptiste.demorgny.etu@univ-lille.fr>
2023-09-30 11:11:01 +0200

    sauv

--- essai.py
+++ essai.py
@@ -46,10 +46,46 @@ df2 = pd.read_csv('fichiers_concatenes2.csv')
 # Utilisez la fonction head() pour afficher un aperçu des premières lignes du DataFrame
 
 # Affichez l'aperçu
-print(df.head())
+'''print(df.head())
 print(df2.head())
 
 df2 = df2.rename(columns=df.columns)
 print(df.head())
 print(df2.head())
+'''
 
+'''
+créer une onction clean qui prend un dataframe et retourn un datafram
+- mettre le nom des colonne en minuscule, remplace espace par under score
+- caster la colonne order date en datetime (pd todatetime)
+-renommer la colonne order_id en order number
+creer la colonne chiffre d'affaire à maille order id
+'''
+def extract(data_dir, prefix, start_week, end_week):
+    """ Extract a temporal slice of data for a given data source.
+    
+    Parameters
+    ----------
+    data_dir: str
+        Data directory path.
+    start_week: int
+        First week number (included)
+    end_week: int
+        Last week number (included)
+    prefix: str
+        Data source identification (e.g. restaurant_1)
+    """
+    df = pd.DataFrame()
+    
+    for i in range(start_week, end_week+1):
+        file_path = os.path.join(data_dir, 'data', f'{prefix}_week_{i}.csv')
+
+        if os.path.isfile(file_path):
+            batch = pd.read_csv(file_path)
+            df = pd.concat([df, batch], sort=True)
+    
+    return df
+df1 = extract(data_dir= "C:/Users/277775/MLops/data",
+       prefix="restaurant_1" , start_week=108, end_week=110)
+
+df1.head()
